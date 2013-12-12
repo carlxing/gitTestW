158d734c05ba623fdc3f93f6507170c6a7bbe832
john.xing <iloveyou62888@126.com>
2013-12-12 17:27:37 +0800

    First commit

---
+++ src/main/java/Main1.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: john.xing
+ * Date: 13-12-12
+ * Time: 下午5:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main1 {
+}

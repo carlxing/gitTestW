ff4fff44be994a3f991f2ccd6d5600a78b08cef9
john.xing <iloveyou62888@126.com>
2013-12-12 17:30:56 +0800

    测试1

--- src/main/java/Main1.java
+++ src/main/java/Main1.java
@@ -1,9 +1,9 @@
-/**
- * Created with IntelliJ IDEA.
- * User: john.xing
- * Date: 13-12-12
- * Time: 下午5:25
- * To change this template use File | Settings | File Templates.
- */
+
 public class Main1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("xxx");
+    }
+
 }

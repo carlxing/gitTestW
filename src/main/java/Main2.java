a2ea8c1f380c3dd9fda1573eb5d2849c4abd835a
john.xing <iloveyou62888@126.com>
2013-12-12 18:02:32 +0800

    hasM1andM2

---
+++ src/main/java/Main2.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: john.xing
+ * Date: 13-12-12
+ * Time: 下午6:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main2 {
+
+    public static void main(String[] args) {
+        System.out.println("222");
+    }
+}
